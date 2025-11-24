a4695bb361847abbd4d12730d25db4ee278c9afe
KarimaChami <karimachami24@gmail.com>
2025-11-24 16:41:04 +0100

    main.py - auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,9 @@
 #logique de Securite
+
+
+
+
+def verify_password():
+    return True
+def create_access_token():
+    return "token"
---
+++ crud.py
@@ -0,0 +1,10 @@
+
+
+
+
+
+def get_user_by_username():
+    return None
+
+def create_user():
+    return None
